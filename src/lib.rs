6ad51b6b4c43b9a26e8777d28a1f0d9f810834fe
Johnny Nguyen <thisisjohnny@gmail.com>
2023-08-16 06:57:23 -0500

    refactor lib.rs to include generate_point method.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
+use hex;
 use num_bigint::{BigUint, RandBigInt};
 use rand;
 
-pub struct chaum_pedersen {
+pub struct ChaumPedersen {
     p: BigUint,
     q: BigUint,
     alpha: BigUint,
     beta: BigUint,
 }
 
-impl chaum_pedersen {
+impl ChaumPedersen {
+    /// output = (alpha^exp mod p, beta^exp mod p)
+    pub fn compute_pair(&self, exp: &BigUint) -> (BigUint, BigUint) {
+        let p1 = self.alpha.modpow(exp, &self.p);
+        let p2 = self.beta.modpow(exp, &self.p);
+        (p1, p2)
+    }
+
     /// alpha^x mod p
     /// output = n^exp mod p
     pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
@@ -69,21 +77,21 @@ mod test {
         let k = BigUint::from(7u32);
         let c = BigUint::from(4u32);
 
-        let cp = chaum_pedersen {
+        let cp = ChaumPedersen {
             p: p.clone(),
             q,
             alpha: alpha.clone(),
             beta: beta.clone(),
         };
 
-        let y1 = chaum_pedersen::exponentiate(&alpha, &x, &p);
-        let y2 = chaum_pedersen::exponentiate(&beta, &x, &p);
+        let y1 = ChaumPedersen::exponentiate(&alpha, &x, &p);
+        let y2 = ChaumPedersen::exponentiate(&beta, &x, &p);
 
         assert_eq!(y1, BigUint::from(2u32));
         assert_eq!(y2, BigUint::from(3u32));
 
-        let r1 = chaum_pedersen::exponentiate(&alpha, &k, &p);
-        let r2 = chaum_pedersen::exponentiate(&beta, &k, &p);
+        let r1 = ChaumPedersen::exponentiate(&alpha, &k, &p);
+        let r2 = ChaumPedersen::exponentiate(&beta, &k, &p);
 
         assert_eq!(r1, BigUint::from(8u32));
         assert_eq!(r2, BigUint::from(4u32));
@@ -110,24 +118,84 @@ mod test {
         let p = BigUint::from(23u32);
         let q = BigUint::from(11u32);
         let x = BigUint::from(6u32);
-        let k = chaum_pedersen::generate_random_below(&q);
-        let c = chaum_pedersen::generate_random_below(&q);
+        let k = ChaumPedersen::generate_random_below(&q);
+        let c = ChaumPedersen::generate_random_below(&q);
 
-        let cp = chaum_pedersen {
+        let cp = ChaumPedersen {
             p: p.clone(),
             q,
             alpha: alpha.clone(),
             beta: beta.clone(),
         };
 
-        let y1 = chaum_pedersen::exponentiate(&alpha, &x, &p);
-        let y2 = chaum_pedersen::exponentiate(&beta, &x, &p);
+        let y1 = ChaumPedersen::exponentiate(&alpha, &x, &p);
+        let y2 = ChaumPedersen::exponentiate(&beta, &x, &p);
 
         assert_eq!(y1, BigUint::from(2u32));
         assert_eq!(y2, BigUint::from(3u32));
 
-        let r1 = chaum_pedersen::exponentiate(&alpha, &k, &p);
-        let r2 = chaum_pedersen::exponentiate(&beta, &k, &p);
+        let r1 = ChaumPedersen::exponentiate(&alpha, &k, &p);
+        let r2 = ChaumPedersen::exponentiate(&beta, &k, &p);
+
+        let s = cp.solve(&k, &c, &x);
+
+        let result = cp.verify(&y1, &y2, &r1, &r2, &c, &s);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_1024_bits_constants() {
+        //
+        //    Reference: https://www.rfc-editor.org/rfc/rfc5114#page-15
+        //
+        //    The hexadecimal value of the prime is:
+        //
+        //    p = B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6
+        //        9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0
+        //        13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70
+        //        98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0
+        //        A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708
+        //        DF1FB2BC 2E4A4371
+        //
+        //    The hexadecimal value of the generator is:
+        //
+        //    g = A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F
+        //        D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213
+        //        160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1
+        //        909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A
+        //        D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24
+        //        855E6EEB 22B3B2E5
+        //
+        //    The generator generates a prime-order subgroup of size:
+        //    q = F518AA87 81A8DF27 8ABA4E7D 64B7CB9D 49462353
+        //
+        let p = BigUint::from_bytes_be(&hex::decode("B10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C69A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C013ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD7098488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708DF1FB2BC2E4A4371").unwrap());
+        let q = BigUint::from_bytes_be(
+            &hex::decode("F518AA8781A8DF278ABA4E7D64B7CB9D49462353").unwrap(),
+        );
+
+        let alpha = BigUint::from_bytes_be(
+            &hex::decode("A4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C41564B777E690F5504F213160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28AD662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24855E6EEB22B3B2E5").unwrap(),
+        );
+
+        // beta = alpha^i is also a generator
+        let beta = alpha.modpow(&ChaumPedersen::generate_random_below(&q), &p);
+
+        let cp = ChaumPedersen {
+            p: p.clone(),
+            q: q.clone(),
+            alpha: alpha.clone(),
+            beta: beta.clone(),
+        };
+
+        let x = ChaumPedersen::generate_random_below(&q);
+        let k = ChaumPedersen::generate_random_below(&q);
+
+        let c = ChaumPedersen::generate_random_below(&q);
+
+        let (y1, y2) = cp.compute_pair(&x);
+
+        let (r1, r2) = cp.compute_pair(&k);
 
         let s = cp.solve(&k, &c, &x);
 
